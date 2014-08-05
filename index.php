e6a37fec7a91df13897a759a6ace9c111d4db213
thordan <md5madman@gmail.com>
2014-08-05 16:01:02 -0600

    moved autoload

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
 <?php
-function __autoload($class)
-{
-    $dir = dirname(__FILE__)."/";
-    foreach(array($dir."lib/", $dir."model/", $dir."controller/") as $path)
-    {
-        $path = realpath($path);
-        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
-        foreach($objects as $name => $object)
-        {
-            if(strpos(strtolower($name), strtolower("/$class.class.php")) !== false)
-            {
-                include($name);
-            }
-        }
-    }
-}
-
+require_once('lib/bitmvc.class.php');
 $bitmvc = new BitMvc();
 $bitmvc->run();
--- lib/bitmvc.class.php
+++ lib/bitmvc.class.php
@@ -1,4 +1,21 @@
 <?php
+function __autoload($class)
+{
+    $dir = dirname(__FILE__)."/../";
+    foreach(array($dir."lib/", $dir."model/", $dir."controller/") as $path)
+    {
+        $path = realpath($path);
+        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+        foreach($objects as $name => $object)
+        {
+            if(strpos(strtolower($name), strtolower("/$class.class.php")) !== false)
+            {
+                include($name);
+            }
+        }
+    }
+}
+
 class BitMvc
 {
     public function __construct()
